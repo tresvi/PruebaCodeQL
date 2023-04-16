14b8367da1dfc3c2c6049aea9cf1b5146fabcf50
Raul Palavecino <raulpalav@gmail.com>
2023-04-16 02:04:10 -0300

    Create Circle.cs

---
+++ PruebaCodeQL/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaCodeQL
+{
+    class Circle
+    {
+        private double radius;
+        public double area()
+        {
+            return Math.Pow(radius, 2) * 3.14;
+        }
+        public double circumference()
+        {
+            return radius * 2 * 3.14;
+        }
+    }
+
+}
