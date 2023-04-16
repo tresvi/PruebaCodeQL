eb3391638c4a9251c8586d4a60165f4d4f126777
Raul Palavecino <raulpalav@gmail.com>
2023-04-16 01:30:42 -0300

    Se fuerza error

--- PruebaCodeQL/Program.cs
+++ PruebaCodeQL/Program.cs
@@ -1,10 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-/*Authenticate("usuario", "password");
+Authenticate("usuario", "password");
 
 
 void Authenticate(string user, string password)
 {
     Console.WriteLine("el pass es:" + password);
 }
-*/
