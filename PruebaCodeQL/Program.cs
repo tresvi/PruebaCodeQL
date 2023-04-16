5252e3f1b7780de837f6f4e0cd40b0951cc8881b
Raul Palavecino <raulpalav@gmail.com>
2023-04-16 01:42:44 -0300

    Agregado de TODO

--- PruebaCodeQL/Program.cs
+++ PruebaCodeQL/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+//https://www.youtube.com/watch?v=NjQ9covhknY           //Solucion a error de autobuild
+//https://www.youtube.com/watch?v=9xWW0BwopDk
+//https://learn.microsoft.com/es-es/dotnet/core/tools/sdk-errors/netsdk1005
+//https://stphnwlsh.medium.com/using-github-actions-to-update-net-dependencies-automatically-d555f629fed0
+
+//TODO: Esto es un TO DO
+
 Console.WriteLine("Hello, World!");
 Authenticate("usuario", "password");
 
